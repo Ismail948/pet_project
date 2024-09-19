7d2de358423e4da563cf2b679aff0afa2c91efef
Ismail948 <120742683+Ismail948@users.noreply.github.com>
2024-09-19 22:54:14 +0530

    Delete src/main directory

--- src/main/java/Application/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package Application;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Long> {
-	Optional<Video> findByFilename(String filename);
-}
--- src/main/java/Application/petrepo.java
+++
@@ -1,9 +0,0 @@
-package Application;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface petrepo extends JpaRepository<pettype, Long> {
-
-}
