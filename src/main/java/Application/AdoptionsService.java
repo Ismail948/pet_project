d7e82f9f40f71444fb7556e3da5fc3fcb2b56ca3
Ismail948 <120742683+Ismail948@users.noreply.github.com>
2024-09-19 18:50:38 +0530

    s
    s

---
+++ src/main/java/Application/AdoptionsService.java
@@ -0,0 +1,16 @@
+package Application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class AdoptionsService {
+
+    @Autowired
+    private AdoptionsRepository adoptionsRepository;
+
+    public List<Adoptions> getAvailableAdoptions() {
+        return adoptionsRepository.findByStatus(true); // true indicates available
+    }
+}
